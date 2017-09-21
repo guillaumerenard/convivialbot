e50be3399ffc4aca2b716c2d00ce178fc0964335
Guillaume RENARD <grenard@expertime.com>
2017-09-21 18:01:13 +0200

    api.ai message

--- src/dialogs/apiaidialog.ts
+++ src/dialogs/apiaidialog.ts
@@ -10,25 +10,30 @@ class ApiAiDialog extends BaseDialog{
             (session, args, next) => {
                 if(args.fulfillment.messages.length > 0) {
                     let apiAiMessage = args.fulfillment.messages[0];
+                    let responseMessage = new builder.Message(session);
+                    let responseMessageAttachments: builder.AttachmentType[] = [];
                     switch(apiAiMessage.type) {
                         case 0:
-                            session.send(apiAiMessage.speech);
+                            responseMessage.text(apiAiMessage.speech);
                             break;
                         case 1:
-                            let message = new builder.Message(session);
-                            message.attachmentLayout(builder.AttachmentLayout.carousel);
-                            let messageAttachments: builder.AttachmentType[] = [];
-                            messageAttachments.push(new builder.HeroCard(session)
+                            responseMessage.attachmentLayout(builder.AttachmentLayout.carousel);
+                            responseMessageAttachments.push(new builder.HeroCard(session)
                                 .title(apiAiMessage.title)
                                 .subtitle(apiAiMessage.subtitle)
                                 .text("")
                                 .images([builder.CardImage.create(session, apiAiMessage.imageUrl)]));
-                            message.attachments(messageAttachments);
-                            session.send(message);
+                            responseMessage.attachments(responseMessageAttachments);
+                            break;
+                        case 3:
+                            responseMessageAttachments.push(new builder.HeroCard(session)
+                                .images([builder.CardImage.create(session, apiAiMessage.imageUrl)]));
+                            responseMessage.attachments(responseMessageAttachments);
                             break;
                         default:
                             break;
                     }
+                    session.send(responseMessage);
                 }
                 session.endDialog();
             }
