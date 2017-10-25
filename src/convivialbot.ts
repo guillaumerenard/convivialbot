d6cc6ec5a3e209b65b736e73f12d8796e57ee057
Guillaume RENARD <grenard@expertime.com>
2017-10-25 15:23:15 +0200

    facebook

--- src/convivialbot.ts
+++ src/convivialbot.ts
@@ -37,8 +37,11 @@ class ConvivialBot {
             {
                 botbuilder: function (session, next) {
                     if (session.message.source === "facebook") {
+                        session.send(session.message.source);
                         if (session.message.sourceEvent && session.message.sourceEvent.message) {
+                            session.send(session.message.sourceEvent.message);
                             if (session.message.sourceEvent.message.quick_reply) {
+                                session.send(session.message.sourceEvent.quick_reply);
                                 session.message.text = session.message.sourceEvent.message.quick_reply.payload;
                             }
                         }
