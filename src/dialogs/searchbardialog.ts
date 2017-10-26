fed6b712ae7e6e2908b70da2a6ed2e17aba00dc6
Guillaume RENARD <grenard@expertime.com>
2017-10-26 10:05:37 +0200

    geoloc

--- src/dialogs/searchbardialog.ts
+++ src/dialogs/searchbardialog.ts
@@ -12,7 +12,11 @@ class SearchBarDialog extends BaseDialog{
             (session, args, next) => {
                 session.dialogData.initPromise = this.initDialog(session, args);
                 if(session.dialogData.barCity !== "") {
-                    next();
+                    GoogleService.geocoding(session.dialogData.barCity).then(result => {
+                        session.dialogData.barLatitude = result.geometry.location.lat;
+                        session.dialogData.barLongitude = result.geometry.location.lng;
+                        next();
+                    });
                 }
                 else {
                     session.beginDialog("barCity");
@@ -49,35 +53,29 @@ class SearchBarDialog extends BaseDialog{
             },
             (session, results, next) => {
                 session.send(`I am looking for you to bar in ${session.dialogData.barCity} matching your criteria`);
-                session.send(`${session.dialogData.barLatitude} ${session.dialogData.barLongitude}`);
                 session.sendTyping();
-                session.dialogData.initPromise.then(() => {
-                    session.send(`${session.dialogData.barLatitude} ${session.dialogData.barLongitude}`);
-                    BarService.searchBars(session.dialogData.barLatitude, session.dialogData.barLongitude, session.dialogData.barAtmosphere, session.dialogData.barWithWho).then(searchResults => {
-                        if(searchResults && searchResults.status.foundOutlets > 0) {
-                            session.send(`I found ${searchResults.status.foundOutlets} bars matching your request`);
-                            session.send("Here are the most popular");
-                            let bestResultMessage = new builder.Message(session);
-                            bestResultMessage.attachmentLayout(builder.AttachmentLayout.carousel);
-                            let bestResultAttachments: builder.AttachmentType[] = [];
-                            for(let i:number=0; i < searchResults.status.foundOutlets && i < 5; i++) {
-                                bestResultAttachments.push(new builder.HeroCard(session)
-                                    .title(searchResults.outlets[i].name)
-                                    .subtitle(`${searchResults.outlets[i].address.street} ${searchResults.outlets[i].address.postalCode} ${searchResults.outlets[i].address.city}`)
-                                    .text(`${searchResults.outlets[i].type} phone: ${searchResults.outlets[i].contact.phone}`)
-                                    .images([builder.CardImage.create(session, "https://www.scandichotels.com/imagevault/publishedmedia/suw58cmdyrxfvvjep2a5/Scandic-Malmen-Interior-bar-Lilla-hotellbaren-over.jpg")]));
-                            }
-                            bestResultMessage.attachments(bestResultAttachments);
-                            session.send(bestResultMessage);
-                            builder.Prompts.text(session, "Do you have other criteria for your search ?");
+                BarService.searchBars(session.dialogData.barLatitude, session.dialogData.barLongitude, session.dialogData.barAtmosphere, session.dialogData.barWithWho).then(searchResults => {
+                    if(searchResults && searchResults.status.foundOutlets > 0) {
+                        session.send(`I found ${searchResults.status.foundOutlets} bars matching your request`);
+                        session.send("Here are the most popular");
+                        let bestResultMessage = new builder.Message(session);
+                        bestResultMessage.attachmentLayout(builder.AttachmentLayout.carousel);
+                        let bestResultAttachments: builder.AttachmentType[] = [];
+                        for(let i:number=0; i < searchResults.status.foundOutlets && i < 5; i++) {
+                            bestResultAttachments.push(new builder.HeroCard(session)
+                                .title(searchResults.outlets[i].name)
+                                .subtitle(`${searchResults.outlets[i].address.street} ${searchResults.outlets[i].address.postalCode} ${searchResults.outlets[i].address.city}`)
+                                .text(`${searchResults.outlets[i].type} phone: ${searchResults.outlets[i].contact.phone}`)
+                                .images([builder.CardImage.create(session, "https://www.scandichotels.com/imagevault/publishedmedia/suw58cmdyrxfvvjep2a5/Scandic-Malmen-Interior-bar-Lilla-hotellbaren-over.jpg")]));
                         }
-                        else {
-                            session.send("Sorry, I did not find any bar that matches your criteria");
-                            session.endDialog();
-                        }
-                    });
-                }, reason => {
-                    session.endDialog(reason);
+                        bestResultMessage.attachments(bestResultAttachments);
+                        session.send(bestResultMessage);
+                        builder.Prompts.text(session, "Do you have other criteria for your search ?");
+                    }
+                    else {
+                        session.send("Sorry, I did not find any bar that matches your criteria");
+                        session.endDialog();
+                    }
                 });
             },
             (session, results, next) => {
@@ -106,43 +104,21 @@ class SearchBarDialog extends BaseDialog{
      * @param session 
      * @param args 
      */
-    private initDialog(session: builder.Session, args: any): Promise<null> {
-        return new Promise<null>((resolve, reject) => {
-            session.send("Start init");
-            if(args) {
-                session.send("avec args");
-                if(args.metadata.intentName === "AddSearchBarCriteria") {
-                    args.parameters.BarCity !== "" ? session.dialogData.barCity = args.parameters.BarCity : session.dialogData.barCity = args.parameters.ContextBarCity;
-                    args.parameters.BarAtmosphere !== "" ? session.dialogData.barAtmosphere = args.parameters.BarAtmosphere : session.dialogData.barAtmosphere = args.parameters.ContextBarAtmosphere;
-                    args.parameters.BarWithWho !== "" ? session.dialogData.barWithWho = args.parameters.BarWithWho : session.dialogData.barWithWho = args.parameters.ContextBarAtmosphere;
-                }
-                else {
-                    session.dialogData.barCity = args.parameters.BarCity;
-                    session.dialogData.barAtmosphere = args.parameters.BarAtmosphere;
-                    session.dialogData.barWithWho = args.parameters.BarWithWho;
-                }
-                session.send("end get data");
-                session.dialogData.result = args;
-                session.dialogData.contexts = args.contexts;
-                session.send(session.dialogData.barCity);
-                if(session.dialogData.barCity) {
-                    session.send("geocoding");
-                    GoogleService.geocoding(session.dialogData.barCity).then(result => {
-                        session.dialogData.barLatitude = result.geometry.location.lat;
-                        session.dialogData.barLongitude = result.geometry.location.lng;
-                        resolve();
-                    }, reason => {
-                        reject(reason);
-                    });
-                }
-                else {
-                    resolve();
-                }
+    private initDialog(session: builder.Session, args: any): void {
+        if(args) {
+            if(args.metadata.intentName === "AddSearchBarCriteria") {
+                args.parameters.BarCity !== "" ? session.dialogData.barCity = args.parameters.BarCity : session.dialogData.barCity = args.parameters.ContextBarCity;
+                args.parameters.BarAtmosphere !== "" ? session.dialogData.barAtmosphere = args.parameters.BarAtmosphere : session.dialogData.barAtmosphere = args.parameters.ContextBarAtmosphere;
+                args.parameters.BarWithWho !== "" ? session.dialogData.barWithWho = args.parameters.BarWithWho : session.dialogData.barWithWho = args.parameters.ContextBarAtmosphere;
             }
             else {
-                resolve();
+                session.dialogData.barCity = args.parameters.BarCity;
+                session.dialogData.barAtmosphere = args.parameters.BarAtmosphere;
+                session.dialogData.barWithWho = args.parameters.BarWithWho;
             }
-        });
+            session.dialogData.result = args;
+            session.dialogData.contexts = args.contexts;
+        }
     }
 }
 
